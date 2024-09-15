a5bb2c228d8886d8d9652ace08d6878b95f1acbc
Rohit Singh <singhrohit.14oct@gmail.com>
2024-09-15 23:04:57 +0530

    Contact-Tracing

---
+++ force-app/main/default/aura/CTPersonView/CTPersonViewHelper.js
@@ -0,0 +1,29 @@
+({
+  updateStatus: function (component) {
+    const recordId = component.get("v.recordId");
+
+    const action = component.get("c.updateHealthStatus");
+    action.setParams({
+      personId: recordId
+    });
+
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        //Handle response here - response.getReturnValue();
+        this.showToast("Success", "Person Health Status Updated", "success");
+      }
+    });
+    $A.enqueueAction(action);
+  },
+  showToast: function (title, message, type) {
+    //method definition goes here
+    const toastEvent = $A.get("e.force:showToast");
+    toastEvent.setParams({
+      title,
+      message,
+      type
+    });
+    toastEvent.fire();
+  }
+});
